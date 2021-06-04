b19e90662fcb36338f76a9f0f9c6a8f8ecc7c3b8
vinipai45 <vinipai45@gmail.com>
2021-06-04 14:57:16 +0530

    get images route permission changed

--- routes/animal.js
+++ routes/animal.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { isAdmin, isSignedIn } = require("../controllers/auth");
 const { createAnimal, getAllAnimals } = require("../controllers/animal");
 
-router.get("/animals", isSignedIn, getAllAnimals);
+router.get("/animals", getAllAnimals);
 router.post("/create-animal", isSignedIn, isAdmin, createAnimal);
 
 module.exports = router;
